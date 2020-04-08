20e0e888a732fe8ff14b2fa2c7e5c288e986009e
remigerme <remi.germe@gmail.com>
2020-04-08 23:25:55 +0200

    Creating and describing first prototype.

---
+++ prototype.py
@@ -0,0 +1,14 @@
+"""
+First prototype of the project.
+We have two blocs :
+	- that are square (no matter their shape in this prototype)
+	- whose the mass, the initial velocity and position are given (no acceleration)
+	- moving along the same and unique horizontal axis
+	- there is no loss of energy when blocks collide together or with a wall
+	- there can be friction with the ground only (not air)
+
+We can :
+	- activate / desactivate the side walls
+	- make the two blocs collide each other, and know what their velocity will be
+	- obviously count the number of collisions between the blocks, if only the left side wall is enabled
+"""
