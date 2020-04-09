deef038e71867a5ab68731206265c7a1510f89e1
remigerme <remi.germe@gmail.com>
2020-04-09 14:21:27 +0200

    Adding Wall class

--- prototype.py
+++ prototype.py
@@ -14,6 +14,39 @@ We can :
 """
 
 
+import math
+import pygame
+
+FPS = 240 # it will compute 240 times per second the position of the blocks
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+
+class Wall:
+	"""
+	This class allows us to create walls, with which blocks will interact.
+	"""
+	def __init__(self, pos, size):
+		self.x = pos[0]
+		self.y = pos[1]
+		self.thickness = size[0]
+		self.height = size[1]
+		self.rect = pygame.Rect((self.x, self.y), (self.thickness, self.height))
+		self.img = pygame.Surface((self.thickness, self.height))
+		self.img.fill(WHITE)
+
+	def collide(self, block):
+		# as we have only one horizontal axis, we don't matter of the vertical axis
+		if block.vx > 0:
+			if self.x <= block.x + block.size <= self.x + self.thickness:
+				return True
+		else:
+			if self.x <= block.x <= self.x + self.thickness
+				return True
+		return False
+
+
 class Block:
 	"""
 	This class allows us to simply create and manage blocks.
